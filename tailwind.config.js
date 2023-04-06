ff63c54344f9573d8d1c5c2f224055889629b30d
kilian-nagel <nagelkilian05@gmail.com>
2023-04-06 18:19:31 +0200

    refactor : la configuration ne prennait pas en compte les fichiers html présentes dans le dossier page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./index.html'],
+	content: ['./index.html', './pages/**/*.html'],
 	presets: [],
 	darkMode: 'media', // or 'class'
 	theme: {
